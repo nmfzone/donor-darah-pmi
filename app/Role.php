d1d7016ec50060a89e7b14272fd3b311a9fafcbc
nmfzone <nabilftd@gmail.com>
2015-07-13 22:13:26 +0700

    Added the static roles

--- app/Role.php
+++ app/Role.php
@@ -5,6 +5,12 @@ namespace DonorDarahPMI;
 class Role
 {
 
+	public static $roles = [
+		'ADMINPUSAT'  => 'AdminPusat',
+		'ADMINDAERAH' => 'AdminDaerah',
+		'ANGGOTA'     => 'Anggota'
+    ];
+
 	const ADMINPUSAT  = 'ADMINPUSAT';
 	const ADMINDAERAH = 'ADMINDAERAH';
 	const ANGGOTA     = 'ANGGOTA';
